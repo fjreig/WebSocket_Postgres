e4b652f6842799820edb08d147bf8d538b375546
FJ Reig <fjreig@pavener.com>
2025-02-19 13:27:47 +0100

    cambios

--- app/main.py
+++ app/main.py
@@ -17,20 +17,18 @@ with open('app/info.md') as f:
 
 msgs = []
 @rt('/')
-def home(): return Div(
-    Div(Ul(*[Li(m) for m in msgs], id='msg-list')),
-    Form(Input(id='msg'), id='form', ws_send=True),
-    hx_ext='ws', ws_connect='/ws')
+def home():
+    cts = Div(hx_ext='ws', ws_connect='/ws')(
+        Div(Ul(*[Li(m) for m in msgs], id='msg-list')),
+        Form(Input(id='msg'), id='form', ws_send=True)
+    )
+    return Titled('Websocket Test', cts)
 
-users = {}
-def on_conn(ws, send): users[str(id(ws))] = send
-def on_disconn(ws): users.pop(str(id(ws)), None)
-
-@app.ws('/ws', conn=on_conn, disconn=on_disconn)
 async def ws(msg:str):
     msgs.append(msg)
-    # Use associated `send` function to send message to each user
-    for u in users.values(): await u(Ul(*[Li(m) for m in msgs], id='msg-list'))
+    await send(Ul(*[Li(m) for m in msgs], id='msg-list'))
+
+send = setup_ws(app, ws)
 
 @app.get("/Infome_Mensual/{year}/{month}")
 def informe_mensual(year: int, month: int):
